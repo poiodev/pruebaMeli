135684d560743a95b8062154669648ab786abcd5
“poiodev” <valenmaldonado099@gmail.com>
2023-11-10 23:06:13 -0500

    Update 10/11/23

--- src/controller/team.js
+++ src/controller/team.js
@@ -17,13 +17,23 @@ const getStartingTeam = async (req, res) => {
       .json({ message: "No hay suficiente información de entrenamientos" });
   }
 
+  let results = [];
+
   for (let i = 0; i < trainings[0].players.length; i++) {
     const player = trainings[0].players[i];
-    const shootingPower = (player.speed + player.shootingPower) * 0.2;
-    // const speed = player.speed *
+    const shootingPower = (player.speed_km_h + player.shootingPower_km_h) * 0.2;
+    const speed = player.speed_km_h * 0.3;
+    const passes = player.passes * 0.5;
+    const result = shootingPower + speed + passes;
+
+    results.push({ player: player.name, result });
   }
 
-  res.status(200).json({ message: "funciona 2" });
+  results.sort((a, b) => b.result - a.result);
+
+  const finalTeam = results.slice(0, 5);
+
+  res.status(200).json({ finalTeam });
 };
 
 export default { setTraining, getStartingTeam };
